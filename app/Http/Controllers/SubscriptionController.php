1d1b518487e9e1b4b61f1ac0e53ad5ef2bf97f4c
thangzaliana <riomiyachi.1991@gmail.com>
2021-07-02 13:14:46 +0530

    Update SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -83,6 +83,7 @@ class SubscriptionController extends Controller
                 ]);
             $result = json_decode($response->body(), true);
 
+            dd($result);
             DB::beginTransaction();
 
             $currentUser = Auth::user();
